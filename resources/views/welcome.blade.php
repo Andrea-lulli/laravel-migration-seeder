166764b8073b35515d89b26df3f1ac4453b1ecb0
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-01-17 16:58:33 +0100

    stampata lista treni

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,19 @@
 @section('main-content')
 
 @foreach ($trains as $elem )
-        <ul>
-            <li>{{$elem->vettore}}</li>
-            <li>{{$elem->treno}}</li>
-            <li>{{$elem->partenza}}</li>
-            <li>{{$elem->destinazione}}</li>
-            <li>{{$elem->orario}}</li>
-            <li>{{$elem->binario}}</li>
-            <li>{{$elem->informazioni}}</li>
-            <li>{{$elem->ritardo}}</li>
-        </ul>
+<div >
+    <ul class="box">
+        <li>{{$elem->vettore}}</li>
+        <li>{{$elem->treno}}</li>
+        <li>{{$elem->partenza}}</li>
+        <li>{{$elem->destinazione}}</li>
+        <li>{{$elem->orario}}</li>
+        <li>{{$elem->binario}}</li>
+        <li>{{$elem->informazioni}}</li>
+        <li>{{$elem->ritardo}}</li>
+    </ul>
+</div>
+
     @endforeach
 
 @endsection
